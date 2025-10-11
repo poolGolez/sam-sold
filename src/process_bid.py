91f39070823e05cfdb8daa18e35ed6c41d0db93b
Pool Golez <loop.edward@gmail.com>
2025-10-11 18:32:17 +0800

    Find lot using ID

--- src/process_bid.py
+++ src/process_bid.py
@@ -2,12 +2,13 @@ import json
 import os
 from datetime import datetime
 from decimal import Decimal
+from typing import Optional
 
 import boto3
 from aws_lambda_powertools import Logger
 from aws_xray_sdk.core.lambda_launcher import LambdaContext
 
-from domain import Bid
+from domain import Bid, Lot, LotStatus
 
 bids_table_name = os.environ['BIDS_TABLE']
 dynamodb = boto3.resource('dynamodb')
@@ -25,7 +26,12 @@ def parse_record(record: dict) -> Bid:
 
 
 def save_bid(bid):
-    # Lot should exist
+    lot = find_lot(bid.lot_id)
+    if lot is None:
+        raise ValueError(f"Lot {bid.lot_id} does not exists")
+
+    # Lot should be ACTIVE!
+
     if bid.amount < 0:
         raise ValueError("Amount should not be negative")
 
@@ -36,6 +42,28 @@ def save_bid(bid):
         },
         ConditionExpression='attribute_not_exists(PK)'
     )
+
+
+def find_lot(lot_id: str) -> Optional[Lot]:
+    response = bids_table.get_item(Key={
+        "PK": f"LOT#{lot_id}"
+    })
+
+    item = response.get('Item')
+    if item is None:
+        return None
+
+    lot = Lot(
+        id=item['id'],
+        name=item['name'],
+        status=LotStatus[item['status']],
+        highest_bid_id=item.get('highest_bid_id'),
+        highest_bid_amount=item.get('highest_bid_amount'),
+        time_opened=datetime.fromisoformat(item['time_opened']) if item.get('time_opened') is not None else None,
+        time_closed=datetime.fromisoformat(item['time_closed']) if item.get('time_closed') is not None else None
+    )
+
+    return lot
 
 
 @logger.inject_lambda_context
