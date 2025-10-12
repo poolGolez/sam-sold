3593487960f3eeb7cea158444677366e09624c4c
Pool Golez <loop.edward@gmail.com>
2025-10-12 14:38:02 +0800

    Implement API to fetch specific lot

--- src/lot_api.py
+++ src/lot_api.py
@@ -1,17 +1,29 @@
+import os
+
+import boto3
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
+from dao import find_lot
+
 app = APIGatewayRestResolver()
 logger = Logger(service="Lot API")
-
+bids_table_name = os.environ['BIDS_TABLE']
+dynamodb = boto3.resource('dynamodb')
+bids_table = dynamodb.Table(bids_table_name)
 
 @app.get("/lots/<lot_id>")
 def get_lot(lot_id: str):
+    lot = find_lot(bids_table, lot_id)
     return {
-        "lot_id": lot_id,
-        "lot": "<TODO>"
-    }
+        "id": lot.id,
+        "name": lot.name,
+        "status": lot.status.name,
+        "highest_bid_id": lot.highest_bid_id,
+        "highest_bid_amount": lot.highest_bid_amount,
+        "time_opened": lot.time_opened.isoformat() if lot.time_opened is not None else None,
+    } if lot else None
 
 
 @logger.inject_lambda_context
