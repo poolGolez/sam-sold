a83b381387a9f3837a20ddc659d4d313cf3277a9
Pool Golez <loop.edward@gmail.com>
2025-10-21 22:30:51 +0800

    Filter dynamoDB stream processing only to Lot changes

--- src/broadcast_lot_changes.py
+++ src/broadcast_lot_changes.py
@@ -10,9 +10,10 @@ def lambda_handler(event, context):
         new_image = record['dynamodb'].get('NewImage', {})
         old_image = record['dynamodb'].get('OldImage', {})
 
-        print(f"Event: {event_name}")
-        print(f"Keys: {keys}")
-        print(f"Old Image: {old_image}")
-        print(f"New Image: {new_image}")
+        # print(f"Event: {event_name}")
+        # print(f"Keys: {keys}")
+        # print(f"Old Image: {old_image}")
+        # print(f"New Image: {new_image}")
+        logger.info(f"{new_image['PK']['S']} bid increased to {new_image.get('highest_bid_amount', {}).get('N', 0.0)}")
 
     return {"statusCode": 200}
