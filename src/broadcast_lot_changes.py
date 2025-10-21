bf22b2cad2d03bdfd6f38213984b46d8328892e4
Pool Golez <loop.edward@gmail.com>
2025-10-21 22:08:18 +0800

    Initial implementation for consuming DynamoDb streams via Lambda

---
+++ src/broadcast_lot_changes.py
@@ -0,0 +1,18 @@
+from aws_lambda_powertools import Logger
+
+logger = Logger(service="BroadcastLotChangesService")
+
+@logger.inject_lambda_context
+def lambda_handler(event, context):
+    for record in event['Records']:
+        event_name = record['eventName']
+        keys = record['dynamodb']['Keys']
+        new_image = record['dynamodb'].get('NewImage', {})
+        old_image = record['dynamodb'].get('OldImage', {})
+
+        print(f"Event: {event_name}")
+        print(f"Keys: {keys}")
+        print(f"Old Image: {old_image}")
+        print(f"New Image: {new_image}")
+
+    return {"statusCode": 200}
