a6fe8137f6b4bf64c49d1b321f720a7db2eba73e
Pool Golez <loop.edward@gmail.com>
2025-10-26 13:23:48 +0800

    Change structure of saving connections to allow indexing

--- src/lot_notification/ws_connection.py
+++ src/lot_notification/ws_connection.py
@@ -14,15 +14,34 @@ logger = Logger(service='ConnectionService')
 @logger.inject_lambda_context
 def lambda_handler(event, _context):
     logger.info("Received event:", extra={'event': event})
-
     details = event['requestContext']
     connection_id = details['connectionId']
-    user_id = event['queryStringParameters']['user']
 
-    connections_table.put_item(Item={
-        'PK': 'LOT#1024',
-        'SK': connection_id,
-        'user_id': user_id,
-        'details': json.dumps(details),
-    })
+    query_params = event['queryStringParameters']
+    user_id = query_params.get('user-id', None)
+    lot_id = query_params.get('lot-id', None)
+
+    if user_id is None:
+        logger.error("Missing query param: user-id", extra={'query': query_params})
+        return {'statusCode': 400}
+
+    if lot_id is None:
+        logger.error("Missing query param: lot-id", extra={'query': query_params})
+        return {'statusCode': 400}
+
+    connection_key = f"CONN#{connection_id}"
+    items_to_write = [
+        {'PK': f"LOT#{lot_id}", 'SK': connection_key, 'connection_id': connection_id},
+        {
+            'PK': connection_key,
+            'SK': connection_key,
+            'connection_id': connection_id,
+            'user_id': user_id,
+            'details': json.dumps(details),
+        }
+    ]
+
+    with connections_table.batch_writer() as batch:
+        for item in items_to_write:
+            batch.put_item(Item=item)
     return {"statusCode": 200}
