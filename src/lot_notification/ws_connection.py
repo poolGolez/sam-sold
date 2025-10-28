846b6123555c30bf4cf4ae81bb47a30329e49bcc
Pool Golez <loop.edward@gmail.com>
2025-10-28 19:31:36 +0800

    Add $disconnect shit

--- src/lot_notification/ws_connection.py
+++ src/lot_notification/ws_connection.py
@@ -11,24 +11,20 @@ connections_table = dynamodb.Table(connections_table_name)
 logger = Logger(service='ConnectionService')
 
 
-@logger.inject_lambda_context
-def lambda_handler(event, _context):
-    logger.info("Received event:", extra={'event': event})
-    details = event['requestContext']
-    connection_id = details['connectionId']
-
+def on_connect(event):
     query_params = event['queryStringParameters']
     user_id = query_params.get('user-id', None)
     lot_id = query_params.get('lot-id', None)
-
     if user_id is None:
         logger.error("Missing query param: user-id", extra={'query': query_params})
-        return {'statusCode': 400}
+        raise ValueError("Missing query param: lot-id")
 
     if lot_id is None:
         logger.error("Missing query param: lot-id", extra={'query': query_params})
-        return {'statusCode': 400}
+        raise ValueError("Missing query param: lot-id")
 
+    details = event['requestContext']
+    connection_id = details['connectionId']
     connection_key = f"CONN#{connection_id}"
     items_to_write = [
         {'PK': f"LOT#{lot_id}", 'SK': connection_key, 'connection_id': connection_id},
@@ -44,4 +40,28 @@ def lambda_handler(event, _context):
     with connections_table.batch_writer() as batch:
         for item in items_to_write:
             batch.put_item(Item=item)
+
+def on_disconnect(event):
+    connection_id = event['requestContext']['connectionId']
+    logger.info(f"Disconnecting {connection_id}", extra={"event": event})
+    connection_key = f"CONN#{connection_id}"
+    # TODO: Retrieve from DynamoDB
+    lot_id = "1024" # Fuck, needs to  read from DB
+    lot_key = f"LOT#{lot_id}"
+    items_to_delete = [
+        {'PK': connection_key, 'SK': connection_key},
+        {'PK': lot_key, 'SK': connection_key},
+    ]
+
+    with connections_table.batch_writer() as batch:
+        for key in items_to_delete:
+            batch.delete_item(Key=key)
+
+# TODO on disconnect, delete connection
+@logger.inject_lambda_context
+def lambda_handler(event, _context):
+    logger.info("Received event:", extra={'event': event})
+    event_type = event['requestContext']['eventType']
+    if event_type == 'CONNECT':
+        on_connect(event)
     return {"statusCode": 200}
