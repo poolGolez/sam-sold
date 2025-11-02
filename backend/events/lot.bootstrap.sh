1e0b19c94d0e9d5d2ff7f841cd2d0b8512181b9d
Pool Golez <loop.edward@gmail.com>
2025-11-02 12:09:54 +0800

    Add more lots

--- backend/events/lot.bootstrap.sh
+++ backend/events/lot.bootstrap.sh
@@ -1,28 +1,76 @@
-aws dynamodb put-item \
-  --table-name 'SamSold-Bids' \
-  --item '{
-   "PK": {"S": "LOT#1024"},
-   "id": {"S": "1024"},
-   "name": {"S": "Shiny Magmar"},
-   "status": {"S":"OPEN"},
-   "time_opened": {"S":"2025-02-18T06:37:59.03365"}
-  }'
-
-aws dynamodb put-item \
-  --table-name 'SamSold-Bids' \
-  --item '{
-   "PK": {"S": "LOT#512"},
-   "id": {"S": "512"},
-   "name": {"S": "Ghostly Magikarp"},
-   "status": {"S":"OPEN"},
-   "time_opened": {"S":"2023-12-21T10:37:55.826744"}
-  }'
-
-aws dynamodb put-item \
-  --table-name 'SamSold-Bids' \
-  --item '{
-   "PK": {"S": "LOT#32"},
-   "id": {"S": "32"},
-   "name": {"S": "Dark Gardevoir"},
-   "status": {"S":"DRAFT"}
-  }'
+aws dynamodb batch-write-item --request-items '{
+  "SamSold-Bids": [
+    {
+      "PutRequest": {
+        "Item": {
+          "PK": {"S": "LOT#1024"},
+          "id": {"S": "1024"},
+          "name": {"S": "Shiny Magmar"},
+          "status": {"S":"OPEN"},
+          "time_opened": {"S":"2025-02-18T06:37:59.03365"},
+          "imageUrl": {"S": "images/pokemon/shiny-magmar.webp"}
+        }
+      }
+    },
+    {
+      "PutRequest": {
+        "Item": {
+          "PK": {"S": "LOT#1025"},
+          "id": {"S": "1025"},
+          "name": {"S": "Galarian Ponyta"},
+          "status": {"S":"OPEN"},
+          "time_opened": {"S":"2025-02-18T06:37:59.03365"},
+          "imageUrl": {"S": "images/pokemon/galarian-ponyta.png"}
+        }
+      }
+    },
+    {
+      "PutRequest": {
+        "Item": {
+          "PK": {"S": "LOT#1026"},
+          "id": {"S": "1026"},
+          "name": {"S": "Pokemon Egg"},
+          "status": {"S":"OPEN"},
+          "time_opened": {"S":"2025-02-18T06:37:59.03365"},
+          "imageUrl": {"S": "images/pokemon/pokemon-egg.jpeg"}
+        }
+      }
+    },
+    {
+      "PutRequest": {
+        "Item": {
+          "PK": {"S": "LOT#1027"},
+          "id": {"S": "1027"},
+          "name": {"S": "Celebi"},
+          "status": {"S":"OPEN"},
+          "time_opened": {"S":"2025-02-18T06:37:59.03365"},
+          "imageUrl": {"S": "images/pokemon/celebi.webp"}
+        }
+      }
+    },
+    {
+      "PutRequest": {
+        "Item": {
+          "PK": {"S": "LOT#1028"},
+          "id": {"S": "1028"},
+          "name": {"S": "Dark Mimikyu"},
+          "status": {"S":"OPEN"},
+          "time_opened": {"S":"2025-02-18T06:37:59.03365"},
+          "imageUrl": {"S": "images/pokemon/dark-mimikyu.png"}
+        }
+      }
+    },
+    {
+      "PutRequest": {
+        "Item": {
+          "PK": {"S": "LOT#1029"},
+          "id": {"S": "1029"},
+          "name": {"S": "Shiny Gardevoir"},
+          "status": {"S":"OPEN"},
+          "time_opened": {"S":"2025-02-18T06:37:59.03365"},
+          "imageUrl": {"S": "images/pokemon/shiny-gardevoir.png"}
+        }
+      }
+    }
+  ]
+}';
