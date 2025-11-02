a52000eb804cd5d9eda55acaff52e925d599271a
Pool Golez <loop.edward@gmail.com>
2025-11-02 11:14:54 +0800

    Add stub lots

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Container, Grid } from "@mui/material";
+import LotCard from "./components/LotCard";
+
+const lots = [
+  {
+    name: "Shiny Magmar",
+    highestBidAmount: 25.31,
+    imageUrl: "/images/pokemon/shiny-magmar.webp",
+  },
+  {
+    name: "Galarian Ponyta",
+    highestBidAmount: 22.1,
+    imageUrl: "/images/pokemon/galarian-ponyta.png",
+  },
+  {
+    name: "Pokemon Egg",
+    highestBidAmount: 30.45,
+    imageUrl: "/images/pokemon/pokemon-egg.jpeg",
+  },
+  {
+    name: "Celebi",
+    highestBidAmount: 27.89,
+    imageUrl: "/images/pokemon/celebi.webp",
+  },
+  {
+    name: "Dark Mimikyu",
+    highestBidAmount: 35.0,
+    imageUrl: "/images/pokemon/dark-mimikyu.png",
+  },
+  {
+    name: "Shiny Gardevoir",
+    highestBidAmount: 29.75,
+    imageUrl: "/images/pokemon/shiny-gardevoir.png",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Container>
+        <header className="App-header">
+          <img src="pokeball.png" className="App-logo" alt="logo" />
+        </header>
+        <Grid
+          paddingTop={4}
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
         >
-          Learn React
-        </a>
-      </header>
+          {lots.map((lot, index) => (
+            <Grid size={4} key={index}>
+              <LotCard
+                lotName={lot.name}
+                highestBid={lot.highestBidAmount}
+                imagerUrl={lot.imageUrl}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </div>
   );
 }
--- frontend/src/components/LotCard.tsx
+++ frontend/src/components/LotCard.tsx
@@ -4,14 +4,19 @@ import React from "react";
 interface LotCardProps {
   lotName: string;
   highestBid?: number;
+  imagerUrl: string;
 }
 
-const LotCard: React.FC<LotCardProps> = ({ lotName, highestBid = 0.0 }) => {
+const LotCard: React.FC<LotCardProps> = ({
+  lotName,
+  highestBid = 0.0,
+  imagerUrl,
+}) => {
   return (
     <Card sx={{ display: "flex" }}>
       <Box sx={{ display: "flex", flexDirection: "column", width: "75%" }}>
         <CardContent sx={{ flex: "1 0 auto" }}>
-          <Typography component="div" variant="h5">
+          <Typography component="div" variant="h3">
             {highestBid}
           </Typography>
           <Typography
@@ -23,12 +28,19 @@ const LotCard: React.FC<LotCardProps> = ({ lotName, highestBid = 0.0 }) => {
           </Typography>
         </CardContent>
       </Box>
-      <CardMedia
-        component="img"
-        sx={{ width: 151 }}
-        image="/pokeball.png"
-        alt="Pokeball"
-      />
+      <Box
+        sx={{
+          p: 2,
+          border: "1px solid lightgray",
+        }}
+      >
+        <CardMedia
+          component="img"
+          sx={{ objectFit: "contain", height: "160px" }}
+          image={imagerUrl}
+          alt={lotName}
+        />
+      </Box>
     </Card>
   );
 };
